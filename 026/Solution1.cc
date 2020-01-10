77c687e325d34e8a4c9af89baa36910c2321f1b9
高辛格 <xg.gao@tianrang-inc.com>
2020-01-10 17:33:04 +0800

    026

--- 026/Solution1.cc
+++ 026/Solution1.cc
@@ -4,15 +4,8 @@ public:
         if (nums.empty()) return 0;
         int size = nums.size();
         int tmp = nums[size - 1];
-        int count = 1;
-        for (int i = size - 2; i > -1; i--) {
-            if (nums[i] == tmp) {
-                nums.erase(nums.begin() + i); 
-            } else {
-                tmp = nums[i]; 
-                count++;
-            }
-        }
-        return count;
+        for (int i = size - 2; i > -1; i--)
+            if (nums[i] == tmp) nums.erase(nums.begin() + i); else tmp = nums[i];
+        return nums.size();
     }
 };
